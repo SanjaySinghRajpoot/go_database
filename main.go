ef6fd545303078d50f3e52e512755cc9981d68ca
SanjaySinghRajpoot <iamsanjayrajpoot@gmail.com>
2022-12-15 23:37:30 +0530

    feat: employee struct

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 )
 
 type Address struct {
@@ -12,7 +13,7 @@ type Address struct {
 }
 
 type User struct {
-	name    string
+	Name    string
 	Age     json.Number
 	Contact string
 	Company string
@@ -20,5 +21,32 @@ type User struct {
 }
 
 func main() {
+	dir := "./"
 
+	db, er := New(dir, nil)
+	if er {
+		fmt.Println("Error", er)
+	}
+
+	employees := []User{
+		{"Tim", "23", "8899665544", "My tech", Address{"indore", "Madhya Pradesh", "India", "998855663322"}},
+		{"Sam", "24", "9966345544", "techBro", Address{"Chennai", "Chennai", "India", "998812663322"}},
+		{"Tom", "28", "9924345544", "techBro2", Address{"City", "State", "India", "997612663322"}},
+	}
+
+	for _, value := range employees {
+		db.write("users", value.Name, User{
+			Name:    value.Name,
+			Age:     value.Age,
+			Contact: value.Contact,
+			Company: value.Company,
+			Address: value.Address,
+		})
+	}
+
+	records, er := db.ReadAll("users")
+	if er != nil {
+		fmt.Println("Error", er)
+	}
+	fmt.Println(records)
 }
