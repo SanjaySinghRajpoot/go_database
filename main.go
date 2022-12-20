4ad1fb972cf8a704ca9e8fab6059e4c480dd7d3b
SanjaySinghRajpoot <iamsanjayrajpoot@gmail.com>
2022-12-20 18:32:59 +0530

    feat: write method

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"database/sql/driver"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"sync"
+
+	"github.com/jcelliott/lumber"
+	"go.opencensus.io/resource"
+	"gopkg.in/launchdarkly/go-server-sdk.v5/interfaces"
 )
 
 const Version = "1.0.0"
@@ -30,8 +38,78 @@ type Options struct {
 	Logger
 }
 
-func New() {
+func New(dir string, options *Options) (*Driver, error) {
+	dir = filepath.Clean(dir)
 
+	opts := Options{}
+
+	if options != nil {
+		opts = *options
+	}
+
+	if opts.Logger = lumber.NewConsoleLogger((lumber.INFO)
+
+	driver := Driver{
+		dir: dir,
+		mutexes: make(map[string]*sync.Mutex),
+		log: opts.Logger
+	}
+
+	if _, err := os.Stat(dit); err == nil{
+		opts.Logger.Debug("Using '%s' (database already exists) \n", dir)
+		return &driver, nil
+	}
+
+	opts.Logger.Debug("Creating the database at '%s'.....\n", dir)
+	return &driver, os.MkdirAll(dir, 0755)
+}
+
+func (d *Driver) Write(collection, resource string, v interfaces{}) error {
+    if collection == "" {
+		return fmt.Errorf("Missing collection - no place to save record!!") 
+	}
+
+	if resource == "" {
+		return fmt.Errorf("Missing resource - unable to save record!!")
+	}
+
+	mutex := d.getOrCreateMutex(collection)
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	dir := filepath.Join(d.dir, collection)
+	fnlPath := filepath.Join(dir, resource+".json")
+	tmpPath := fnlPath + ".tmp"
+
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
+	}
+
+	b = append(b, byte('\n')) 
+
+	if err := ioutil.WriteFile(tmpPath, b, 0644); err != nil {
+		return err
+	}
+}
+
+func (d *Driver) Read() error {
+
+}
+
+func (d *Driver) ReadAll() {
+
+}
+
+func (d *Driver) getOrCreateMutex() *sync.Mutex {
+
+}
+
+func stat(path string)(fi os.FileInfo, err error){
+	  if fi, err = os.Stat(path); os.IsNotExist(err){
+		fi, err = os.Stat(path + ".json")
+	  }
+
+	  return 
 }
 
 type Address struct {
